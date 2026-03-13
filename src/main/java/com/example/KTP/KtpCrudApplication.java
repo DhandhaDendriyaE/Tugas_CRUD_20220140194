b78a3d91ac96972fc240a36bc19e2ea4bf8c4e9c
“DhandhaDendriyaE” <dhanda.dendriya.ft22@mail.umy.ac.id>
2026-03-13 21:17:53 +0700

    first commit

---
+++ src/main/java/com/example/KTP/KtpCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.KTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtpCrudApplication.class, args);
+	}
+
+}
